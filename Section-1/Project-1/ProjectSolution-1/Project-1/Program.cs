71e6665faf6796dceca74d443bf006d229bfbd3a
Connor House <connorhouse19@gmail.com>
2018-02-06 23:40:46 -0500

    Updated

--- Section-1/Project-1/ProjectSolution-1/Project-1/Program.cs
+++ Section-1/Project-1/ProjectSolution-1/Project-1/Program.cs
@@ -9,7 +9,7 @@ namespace Project_1
         {
     
             Console.Write("What is your first name? ");
-            string firstName = Console.ReadLine();                                // Console input & value assignment (string)       
+            string firstName = Console.ReadLine();                                     
             Console.Write("What is your middle initial? ");
             string middleInitial = Console.ReadLine();
             Console.Write("What is your last name? ");
