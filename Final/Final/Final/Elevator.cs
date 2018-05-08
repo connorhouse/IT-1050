bc98f175bbf48ee15ddf87bc0d957181ed99ebb7
Connor House <connorhouse19@gmail.com>
2018-05-07 23:20:07 -0400

    Final complete!

---
+++ Final/Final/Final/Elevator.cs
@@ -0,0 +1,34 @@
+﻿namespace Final
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double totalWeight = 0;
+            for (int i = 0; i < Occupants.Length; i++)
+            {
+                totalWeight += Occupants[i].GetWeight;
+            }
+            return totalWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            return GetCurrentWeight() > MaxWeight;
+        }
+    }
+}
---
+++ Final/Final/Final/Passenger.cs
@@ -0,0 +1,24 @@
+﻿namespace Final
+{
+    class Passenger
+    {
+        private string Name;
+        private double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string GetName
+        {
+            get { return Name; }
+        }
+
+        public double GetWeight
+        {
+            get { return Weight; }
+        }
+    }    
+}
